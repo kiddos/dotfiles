648411ca41efff01ae057d7d512667e2dc0d44bc
kiddos <kiddo831007@gmail.com>
2018-08-06 00:45:22 +0800

    add update script

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+cp /home/$USER/.bashrc .
+cp /home/$USER/.gitconfig .
+cp /home/$USER/.mkshrc .
+cp /home/$USER/.pam_environment .
+cp /home/$USER/.profile .
+cp /home/$USER/.ros.zsh .
+cp /home/$USER/.tern-project .
+cp /home/$USER/.tmux.conf .
+cp /home/$USER/.zlogin .
+cp /home/$USER/.zprofile .
+cp /home/$USER/.zshrc .
