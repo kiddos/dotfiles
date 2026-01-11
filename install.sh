9464029d422b8cc1897af8da591ce09c7ddfe515
kiddos <kiddo831007@gmail.com>
2026-01-11 23:03:07 +0800

    update install script

--- install.sh
+++ install.sh
@@ -1,26 +1,18 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env sh
 
 rm -rf $HOME/.gitconfig
 ln -s `pwd`/.gitconfig $HOME/.gitconfig 
 
-rm -rf $HOME/.zshrc
-ln -s `pwd`/.zshrc $HOME/.zshrc
-
 rm -rf $HOME/.tmux.conf
 ln -s `pwd`/.tmux.conf $HOME/.tmux.conf
-
-rm -rf $HOME/.alacritty.toml
-ln -s `pwd`/.alacritty.toml $HOME/.alacritty.toml
 
 rm -rf $HOME/.profile
 ln -s `pwd`/.profile $HOME/.profile
 
-rm -rf $HOME/.local/bin/java_check.sh
-ln -s `pwd`/java_check.sh $HOME/.local/bin/java_check.sh
-
 rm -rf $HOME/.oh-my-zsh/custom/themes/kiddos.zsh-theme
 ln -s `pwd`/custom/themes/kiddos.zsh-theme $HOME/.oh-my-zsh/custom/themes/kiddos.zsh-theme
 
+mkdir -p $HOME/.config
 # kitty
 rm -rf $HOME/.config/kitty
 ln -s `pwd`/kitty $HOME/.config/kitty
@@ -30,5 +22,6 @@ rm -rf $HOME/.config/fish
 ln -s `pwd`/fish $HOME/.config/fish
 
 # mise
+mkdir -p $HOME/.config/mise
 rm -rf $HOME/.config/mise/config.toml
 ln -s `pwd`/mise_config.toml $HOME/.config/mise/config.toml
